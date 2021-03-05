a6f4f92c8d5129d86bf31e8eb90523dbca3933cc
acm-sp85 <acontell@gmail.com>
2021-03-05 13:34:39 -0500

    Completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+let employee ={ name: 'Pieter', streetAddress: '50 Saint jonhns pl'};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+
+    let newObject = {...employee};
+    newObject[key]=value;
+    
+
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee,key,value){
+
+    employee[key]=value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee,key){
+
+    let newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+
+    delete employee[key];
+    return employee;
+}
